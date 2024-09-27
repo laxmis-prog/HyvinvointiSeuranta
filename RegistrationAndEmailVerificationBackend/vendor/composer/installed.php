447f81b71bba02c51dce5eb452fa7667355c9c32
Laxmi Sharma <laxmisanuu5@gmail.com>
2024-09-27 12:10:44 +0300

    Tehty composer.json tiedosto

---
+++ RegistrationAndEmailVerificationBackend/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sanu/registration-and-email-verification-backend',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '22cda36fb7b7166a1f745ba04b9437651d317c47',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.1',
+            'version' => '6.9.1.0',
+            'reference' => '039de174cd9c17a8389754d3b877a2ed22743e18',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'sanu/registration-and-email-verification-backend' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '22cda36fb7b7166a1f745ba04b9437651d317c47',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
